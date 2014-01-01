422b1d0d5b52ca47e7a5158b0c4de7aabf1ab795
Steven Shi <3po3po@gmail.com>
2014-01-01 18:08:48 -0500

    initial commit

---
+++ footer.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package Inkness
+ */
+?>
+	</div>
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer row" role="contentinfo">
+	<div class="container">
+		<div id="social-icons" class="col-md-4 col-xs-12">
+			    <?php if ( of_get_option('facebook', true) != "") { ?>
+				 <a href="<?php echo esc_url(of_get_option('facebook', true)); ?>" title="Facebook" ><i class="social-icon fa fa-facebook-square"></i></a>
+	             <?php } ?>
+	            <?php if ( of_get_option('twitter', true) != "") { ?>
+				 <a href="<?php echo esc_url("http://twitter.com/".of_get_option('twitter', true)); ?>" title="Twitter" ><i class="social-icon fa fa-twitter-square"></i></a>
+	             <?php } ?>
+	             <?php if ( of_get_option('google', true) != "") { ?>
+				 <a href="<?php echo esc_url(of_get_option('google', true)); ?>" title="Google Plus" ><i class="social-icon fa fa-google-plus-square"></i></a>
+	             <?php } ?>
+	             <?php if ( of_get_option('feedburner', true) != "") { ?>
+				 <a href="<?php echo esc_url(of_get_option('feedburner', true)); ?>" title="Github" ><i class="social-icon fa fa-github"></i></a>
+	             <?php } ?>
+	             <?php if ( of_get_option('pinterest', true) != "") { ?>
+				 <a href="<?php echo esc_url(of_get_option('pinterest', true)); ?>" title="Pinterest" ><i class="social-icon fa fa-pinterest-square"></i></a>
+	             <?php } ?>
+	             <?php if ( of_get_option('instagram', true) != "") { ?>
+				 <a href="<?php echo esc_url(of_get_option('instagram', true)); ?>" title="Instagram" ><i class="social-icon fa fa-instagram"></i></a>
+	             <?php } ?>
+	             <?php if ( of_get_option('linkedin', true) != "") { ?>
+				 <a href="<?php echo esc_url(of_get_option('linkedin', true)); ?>" title="LinkedIn" ><i class="social-icon fa fa-linkedin-square"></i></a>
+	             <?php } ?>
+	             <?php if ( of_get_option('youtube', true) != "") { ?>
+				 <a href="<?php echo esc_url(of_get_option('youtube', true)); ?>" title="YouTube" ><i class="social-icon fa fa-youtube-square"></i></a>
+	             <?php } ?>
+	             <?php if ( of_get_option('flickr', true) != "") { ?>
+				 <a href="<?php echo esc_url(of_get_option('flickr', true)); ?>" title="Flickr" ><i class="social-icon fa fa-flickr"></i></a>
+	             <?php } ?>
+         
+			</div>
+	
+	<?php if ( of_get_option('credit1', true) == 0 ) { ?>
+		<div class="site-info col-md-8">
+			<?php do_action( 'inkness_credits' ); ?>
+			<?php printf( __( 'Designed With Trips For Five Theme. Find it on %1$s.', 'Github' ), '<a href="http://github.trips45.com/" rel="designer">Github</a>' ); ?>
+		</div> <!-- .site-info -->
+	<?php } ?>	
+		<div id="footertext" class="col-md-7">
+        	<?php
+			if ( (function_exists( 'of_get_option' ) && (of_get_option('footertext2', true) != 1) ) ) {
+			 	echo of_get_option('footertext2', true); } ?>
+        </div>
+	</div>   
+
+
+	</footer><!-- #colophon -->
+	
+</div><!-- #page -->
+
+<?php		
+	if ( (function_exists( 'of_get_option' ) && (of_get_option('footercode1', true) != 1) ) ) {
+			 	echo of_get_option('footercode1', true); } ?>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package Inkness
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title><?php wp_title( '|', true, 'right' ); ?></title>
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+  <script type="text/javascript" src="//use.typekit.net/gsp6ccs.js">
+</script>
+  <script type="text/javascript">
+//<![CDATA[
+  try{Typekit.load();}catch(e){}
+  //]]>
+  </script>
+
+
+<?php wp_head(); ?>
+<!--
+<link rel='stylesheet' id='steve-custom-css' href='http://blog.trips45.com/wp-content/themes/trips45-theme/css/skins/custom.css?ver=3.9' type='text/css' media='all'/></head>
+-->
+<body <?php body_class(); ?>>
+<div id="parallax-bg"></div>
+<div id="page" class="hfeed site">
+	<?php do_action( 'inkness_before' ); ?>
+	<div id="header-top">
+		<header id="masthead" class="site-header row container" role="banner">
+			<div class="site-branding col-md-5 col-xs-12">
+			<?php if((of_get_option('logo', true) != "") && (of_get_option('logo', true) != 1) ) { ?>
+				<h1 class="site-title logo-container"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+				<?php
+				echo "<img class='main_logo' src='".of_get_option('logo', true)."' title='".esc_attr(get_bloginfo( 'name','display' ) )."'></a></h1>";	
+				}
+			else { ?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1> 
+			<?php	
+			}
+			?>
+ <div class="logo-subtitle">
+        	Steve&#039;s Random Musings on Travel and Technology
+         </div>
+			</div>	
+			
+			<div class="default-nav-wrapper col-md-7 col-xs-12"> 	
+		   		<nav id="site-navigation" class="main-navigation" role="navigation">
+	        		<div id="nav-container">
+						<h1 class="menu-toggle"></h1>
+						<div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'inkness' ); ?>"><?php _e( 'Skip to content', 'inkness' ); ?></a></div>
+	
+							<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+	         		</div>  
+			
+			<div id="top-search" >
+				<?php get_search_form(); ?>
+			</div>
+			</nav><!-- #site-navigation -->
+  			
+		  	</div>
+		</header><!-- #masthead -->
+	</div>
+	
+	<div id="header-2">
+		
+	<!--<div class="ribbon"></div>    -->
+
+		<div class="container">
+			<div class="page-divider "></div>
+<div class="container">
+				<?php
+					if ( (function_exists( 'of_get_option' )) && (of_get_option('slidetitle5',true) !=1) ) {
+						if ( ( of_get_option('slider_enabled') != 0 ) && (is_home())  )  
+				{ ?>
+			<div class="slider-wrapper theme-default"> 
+				<div id="slider" class=" nivoSlider">
+		    			<?php
+				  		$slider_flag = false;
+				  		for ($i=1;$i<6;$i++) {
+				  			$caption = ((of_get_option('slidetitle'.$i, true)=="")?"":"#caption_".$i);
+							if ( of_get_option('slide'.$i, true) != "" ) {
+								echo "<a href='".of_get_option('slideurl'.$i, true)."'><img src='".of_get_option('slide'.$i, true)."' title='".$caption."'></a>"; 
+								$slider_flag = true;
+							}
+						}
+						?>  
+		    	</div><!--#slider-->
+		    			<?php for ($i=1;$i<6;$i++) {
+		    				$caption = ((of_get_option('slidetitle'.$i, true)=="")?"":"#caption_".$i);
+		    				if ($caption != "")
+		    				{
+			    				echo "<div id='caption_".$i."' class='nivo-html-caption'><div class='nivocapper'>";
+			    				echo "<a href='".of_get_option('slideurl'.$i, true)."'><div class='slide-title'>".of_get_option('slidetitle'.$i, true)."</div></a>";
+			    				echo "<div class='slide-description'>".of_get_option('slidedesc'.$i, true)."</div>";
+			    				echo "</div></div>";
+		    				}
+		    			}	
+		    	    
+					?>
+		    </div> <!--#slider wrapper-->
+
+    	</div> </div><!--#container-->
+				<?php 
+						}
+					} 	
+				?>
+		<!--<div id="content" class="site-content row clearfix clear">-->
+		<div>
+		<div class="container col-md-12"> 
