079171734a35126add2106ffcd075213761b3279
Steven Shi <3po3po@gmail.com>
2014-01-01 22:45:36 -0500

    fonts

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<!--<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">-->
   <script type="text/javascript" src="//use.typekit.net/gsp6ccs.js">
 </script>
   <script type="text/javascript">
