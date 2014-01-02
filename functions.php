b30df2d34893b42b5934a0a0fccbcf4196b38b4f
Steven Shi <3po3po@gmail.com>
2014-01-01 19:47:21 -0500

    functions

--- functions.php
+++ functions.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Inkness functions and definitions
+ * trips45 functions and definitions
  *
- * @package Inkness
+ * @package trips45
  */
 
 /**
@@ -19,31 +19,31 @@ if ( !function_exists( 'optionsframework_init' ) ) {
 	require_once dirname( __FILE__ ) . '/inc/options-framework.php';
 }
 
-if ( ! function_exists( 'inkness_setup' ) ) :
+if ( ! function_exists( 'trips45_setup' ) ) :
 
-function inkness_setup() {
+function trips45_setup() {
 
-	load_theme_textdomain( 'inkness', get_template_directory() . '/languages' );
+	load_theme_textdomain( 'trips45', get_template_directory() . '/languages' );
 
 	add_theme_support( 'automatic-feed-links' );
 	add_theme_support( 'post-thumbnails' );
 	add_image_size('homepage-banner',750,450,true);
 
 	register_nav_menus( array(
-		'primary' => __( 'Primary Menu', 'inkness' ),
+		'primary' => __( 'Primary Menu', 'trips45' ),
 	) );
 
-	add_theme_support( 'custom-background', apply_filters( 'inkness_custom_background_args', array(
+	add_theme_support( 'custom-background', apply_filters( 'trips45_custom_background_args', array(
 		'default-color' => '#fcfcfc',
 		'default-image' => '',
 	) ) );
 }
-endif; // inkness_setup
-add_action( 'after_setup_theme', 'inkness_setup' );
+endif; // trips45_setup
+add_action( 'after_setup_theme', 'trips45_setup' );
 
-function inkness_widgets_init() {
+function trips45_widgets_init() {
 	register_sidebar( array(
-		'name'          => __( 'Sidebar', 'inkness' ),
+		'name'          => __( 'Sidebar', 'trips45' ),
 		'id'            => 'sidebar-1',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
@@ -51,7 +51,7 @@ function inkness_widgets_init() {
 		'after_title'   => '</h1>',
 	) );
 	register_sidebar( array(
-		'name'          => __( 'Footer Left', 'inkness' ),
+		'name'          => __( 'Footer Left', 'trips45' ),
 		'id'            => 'sidebar-2',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
@@ -59,7 +59,7 @@ function inkness_widgets_init() {
 		'after_title'   => '</h1>',
 	) );
 	register_sidebar( array(
-		'name'          => __( 'Footer Center', 'inkness' ),
+		'name'          => __( 'Footer Center', 'trips45' ),
 		'id'            => 'sidebar-3',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
@@ -67,7 +67,7 @@ function inkness_widgets_init() {
 		'after_title'   => '</h1>',
 	) );
 	register_sidebar( array(
-		'name'          => __( 'Footer Right', 'inkness' ),
+		'name'          => __( 'Footer Right', 'trips45' ),
 		'id'            => 'sidebar-4',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
@@ -75,11 +75,11 @@ function inkness_widgets_init() {
 		'after_title'   => '</h1>',
 	) );
 }
-add_action( 'widgets_init', 'inkness_widgets_init' );
+add_action( 'widgets_init', 'trips45_widgets_init' );
 
-add_action('optionsframework_custom_scripts', 'inkness_custom_scripts');
+add_action('optionsframework_custom_scripts', 'trips45_custom_scripts');
 
-function inkness_custom_scripts() { ?>
+function trips45_custom_scripts() { ?>
 
 <script type="text/javascript">
 jQuery(document).ready(function() {
@@ -98,55 +98,55 @@ jQuery(document).ready(function() {
 <?php
 }
 
-function inkness_scripts() {
-	//wp_enqueue_style( 'inkness-fonts', '//fonts.googleapis.com/css?family=Open+Sans:300,400,700,600' );
-	wp_enqueue_style( 'inkness-basic-style', get_stylesheet_uri() );
+function trips45_scripts() {
+	//wp_enqueue_style( 'trips45-fonts', '//fonts.googleapis.com/css?family=Open+Sans:300,400,700,600' );
+	wp_enqueue_style( 'trips45-basic-style', get_stylesheet_uri() );
 	if ( (function_exists( 'of_get_option' )) && (of_get_option('sidebar-layout', true) != 1) ) {
 		if (of_get_option('sidebar-layout', true) ==  'right') {
-			wp_enqueue_style( 'inkness-layout', get_template_directory_uri()."/css/layouts/content-sidebar.css" );
+			wp_enqueue_style( 'trips45-layout', get_template_directory_uri()."/css/layouts/content-sidebar.css" );
 		}
 		else {
-			wp_enqueue_style( 'inkness-layout', get_template_directory_uri()."/css/layouts/sidebar-content.css" );
+			wp_enqueue_style( 'trips45-layout', get_template_directory_uri()."/css/layouts/sidebar-content.css" );
 		}	
 	}
 	else {
-		wp_enqueue_style( 'inkness-layout', get_template_directory_uri()."/css/layouts/content-sidebar.css" );
+		wp_enqueue_style( 'trips45-layout', get_template_directory_uri()."/css/layouts/content-sidebar.css" );
 	}
 			
-	wp_enqueue_style( 'inkness-bootstrap-style', get_template_directory_uri()."/css/bootstrap/bootstrap.min.css", array('inkness-layout') );
+	wp_enqueue_style( 'trips45-bootstrap-style', get_template_directory_uri()."/css/bootstrap/bootstrap.min.css", array('trips45-layout') );
 		
-	wp_enqueue_style( 'inkness-main-style', get_template_directory_uri()."/css/skins/main.css", array('inkness-layout','inkness-bootstrap-style') );
+	wp_enqueue_style( 'trips45-main-style', get_template_directory_uri()."/css/skins/main.css", array('trips45-layout','trips45-bootstrap-style') );
 	
-	wp_enqueue_script( 'inkness-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+	wp_enqueue_script( 'trips45-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
-	wp_enqueue_script( 'inkness-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_enqueue_script( 'trips45-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 	if ( (function_exists( 'of_get_option' )) && (of_get_option('slider_enabled') != 0) ) {
-		wp_enqueue_style( 'inkness-nivo-slider-default-theme', get_template_directory_uri()."/css/nivo/slider/themes/default/default.css" );
+		wp_enqueue_style( 'trips45-nivo-slider-default-theme', get_template_directory_uri()."/css/nivo/slider/themes/default/default.css" );
 	
-		wp_enqueue_style( 'inkness-nivo-slider-style', get_template_directory_uri()."/css/nivo/slider/nivo.css" );
+		wp_enqueue_style( 'trips45-nivo-slider-style', get_template_directory_uri()."/css/nivo/slider/nivo.css" );
 	}	
 	
 	if ( (function_exists( 'of_get_option' )) && (of_get_option('slider_enabled') != 0) ) {
-		wp_enqueue_script( 'inkness-nivo-slider', get_template_directory_uri() . '/js/nivo.slider.js', array('jquery') );
+		wp_enqueue_script( 'trips45-nivo-slider', get_template_directory_uri() . '/js/nivo.slider.js', array('jquery') );
 	}
-	wp_enqueue_script( 'inkness-superfish', get_template_directory_uri() . '/js/superfish.js', array('jquery','hoverIntent') );	
+	wp_enqueue_script( 'trips45-superfish', get_template_directory_uri() . '/js/superfish.js', array('jquery','hoverIntent') );	
 	
-	wp_enqueue_script( 'inkness-bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery') );	
+	wp_enqueue_script( 'trips45-bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery') );	
 	
-	wp_enqueue_script( 'inkness-custom-js', get_template_directory_uri() . '/js/custom.js', array('jquery') );
+	wp_enqueue_script( 'trips45-custom-js', get_template_directory_uri() . '/js/custom.js', array('jquery') );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 
 	if ( is_singular() && wp_attachment_is_image() ) {
-		wp_enqueue_script( 'inkness-keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
+		wp_enqueue_script( 'trips45-keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
 	}
-	wp_enqueue_style( 'inkness-steve-custom-style', get_template_directory_uri()."/css/skins/custom.css", array('inkness-layout','inkness-bootstrap-style') );
+	wp_enqueue_style( 'trips45-steve-custom-style', get_template_directory_uri()."/css/skins/custom.css", array('trips45-layout','trips45-bootstrap-style') );
 	
 }
-add_action( 'wp_enqueue_scripts', 'inkness_scripts' );
-function inkness_custom_head_codes() {
+add_action( 'wp_enqueue_scripts', 'trips45_scripts' );
+function trips45_custom_head_codes() {
  if ( (function_exists( 'of_get_option' )) && (of_get_option('headcode1', true) != 1) ) {
 	echo of_get_option('headcode1', true);
  }
@@ -157,16 +157,16 @@ function inkness_custom_head_codes() {
 	echo "<script>jQuery(window).load(function() { jQuery('#slider').nivoSlider({effect:'fade', pauseTime: 4500}); });</script>";
  }
 }	
-add_action('wp_head', 'inkness_custom_head_codes');
+add_action('wp_head', 'trips45_custom_head_codes');
 
-function inkness_nav_menu_args( $args = '' )
+function trips45_nav_menu_args( $args = '' )
 {
     $args['container'] = false;
     return $args;
 } // function
-add_filter( 'wp_page_menu_args', 'inkness_nav_menu_args' );
+add_filter( 'wp_page_menu_args', 'trips45_nav_menu_args' );
 
-function inkness_pagination() {
+function trips45_pagination() {
 	global $wp_query;
 	$big = 12345678;
 	$page_format = paginate_links( array(
