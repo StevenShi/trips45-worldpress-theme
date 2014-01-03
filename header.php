f3364258a09873d1f65d99b4ccf0ad764c43a444
Steven Shi <3po3po@gmail.com>
2014-01-03 08:52:40 -0500

    fixed search style

--- header.php
+++ header.php
@@ -48,18 +48,22 @@
         			<?php bloginfo( 'description' ); ?>
          		</div>
 			</div>	
+			<div class="default-nav-wrapper col-md-2 col-xs-12"> 
+				<p>&nbsp;</p>
+			</div>
 			
-				<div class="default-nav-wrapper col-md-7 col-xs-12"> 	
+				<div class="default-nav-wrapper col-md-5 col-xs-12"> 	
 		   			<nav id="site-navigation" class="main-navigation" role="navigation">
-	        		<div id="nav-container">
-						<h1 class="menu-toggle"></h1>
-						<div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'inkness' ); ?>"><?php _e( 'Skip to content', 'inkness' ); ?></a></div>
-	
-							<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-	         		</div>  
-							
-				<div id="site-navigation" class="main-navigation col-md-6"><gcse:searchbox-only></gcse:searchbox-only></div>
-			</nav><!-- #site-navigation -->
+		        		<div id="nav-container">
+							<h1 class="menu-toggle"></h1>
+							<div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'inkness' ); ?>"><?php _e( 'Skip to content', 'inkness' ); ?></a></div>
+		
+								<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+		         		</div>  
+		         		<div id="nav-container">
+		         			<gcse:searchbox-only></gcse:searchbox-only>
+		         		</div>	
+				</nav><!-- #site-navigation -->
   			
 		  	</div>
 		</header><!-- #masthead -->
